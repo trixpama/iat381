6eeb87acf44805c1dca8e252139bcde9c563660d
trixpama <trixpama@gmail.com>
2015-01-06 12:23:09 -0800

    Lab 1 Challenge

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-$('#clickEvent a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
-})
-
 $('#clickEvent a[href="#home"]').tab('show') // Select tab by name
 $('#clickEvent a[href="#profile"]').tab('show') // Select tab by name
